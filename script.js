a6faae0841e18fe0dd5b2bdafdfe0a9f115ca3d6
McLovin <144797506+sucxcz@users.noreply.github.com>
2023-10-19 23:17:10 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+// function compareNumbers(num1, num2) {
+//     if (num1 > num2) {
+//       return 1;
+//     } else if (num1 < num2) {
+//       return -1;
+//     } else {
+//       return 0;
+//     }
+//   }
+
+//     function myParagraph(backgroundColor, textColor, text) {
+//         var paragraph = document.createElement('p');
+//         paragraph.style.backgroundColor = backgroundColor;
+//         paragraph.style.color = textColor;
+//         paragraph.textContent = text;
+//         document.body.appendChild(paragraph);
+//       }
+
+//       function calculateArea(length, width) {
+//         if (width === undefined) {
+//           return length * length;
+//         } else {
+//           return length * width;
+//         }
+//       }
+
+//       function processNumbers(num1, num2) {
+//         if (num1 % 2 === 0 && num2 % 2 === 0) {
+//           return num1 * num2; 
+//         } else if (num1 % 2 !== 0 && num2 % 2 !== 0) {
+//           return num1 + num2; 
+//         } else {
+//           return num1 % 2 !== 0 ? num1 : num2;
+//         }
+//       }
